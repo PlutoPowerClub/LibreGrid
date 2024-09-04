884125a44bcf0befdd673307b9545d2bef92d805
jones58 <108466184+jones58@users.noreply.github.com>
2024-09-04 17:16:20 +0100

    adding tailwind via npm

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from fasthtml.common import *
 from LibreGrid.fomantic import *
+import argparse
+import subprocess
+
+parser = argparse.ArgumentParser(
+    prog="Fast HTML + Tailwind template",
+    description="FastHTML template using Tailwind CSS for styling",
+)
+parser.add_argument(
+    "--tailwind",
+    type=bool,
+    default=False,
+    help="Run the Tailwind CLI build or not",
+)
+args = parser.parse_args()
+
 
 hdrs = [
     HighlightJS(
@@ -8,8 +23,15 @@ hdrs = [
         ]
     ),
     Meta(name="description", content="LibreGrid: Free and Open Source Community Energy Management System"),
-    Link(href='/assets/styles.css', rel='stylesheet'),
+    Meta(charset="UTF-8"),
+      Meta(
+        name="viewport",
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0",
+    ),
+
+    Link(href='css/input.css', rel='stylesheet'),
     Script(src="https://unpkg.com/htmx.org@2.0.1/dist/htmx.js"),
+    Script(src="https://cdn.tailwindcss.com"),
 ]
 
 app, rt = fast_app(hdrs=fui_hdrs+hdrs, default_hdrs=False)
@@ -17,17 +39,11 @@ app, rt = fast_app(hdrs=fui_hdrs+hdrs, default_hdrs=False)
 @rt('/')
 def get():
     return Div(
-        HeroSegment(
-            "LibreGrid",
-            "Power to the people, literally.",
-        ),
-        VerticalStripeSegment(
-            Grid(
-                Div(
-                    H2("Decentralize power, amplify impact."),
-                    P("We are coding the future of energy management, together."),
-                cls="row")
-            ),
-        ),
-        cls="pusher")
+        "Hello!", cls="font-bold text-red")
+
+if args.tailwind:
+    subprocess.run(
+        ["npx", "tailwindcss", "-i", "css/input.css", "-o", "css/output.css"]
+    )
+
 serve()
